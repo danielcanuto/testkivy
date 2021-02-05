1293edc45b4558ed73950cc63fc5d5c44ef0bb72
Daniel Canuto <danielpscanuto83@gmail.com>
2021-02-05 14:09:01 -0300

    sair android

--- main_.py
+++ main_.py
@@ -4,7 +4,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from kivy.core.window import Window
 
 class Gerenciador(ScreenManager):
     pass
@@ -17,6 +17,17 @@ class Tarefas(Screen):
         super().__init__(**kwargs)
         for tarefa in tarefas:
             self.ids.box.add_widget(Tarefa(text=tarefa))
+    
+    def on_pre_enter(self):
+        Window.bind(on_keyboard=self.voltar)
+    
+    def voltar(self, window, key, *args):
+        if key == 27:
+            App.get_running_app().root.current = "menu"
+            return True
+
+    def on_pre_leave(self):
+        Window.unbind(on_keyboard=self.voltar)
 
     def addWidget(self):
         texto=self.ids.texto.text
